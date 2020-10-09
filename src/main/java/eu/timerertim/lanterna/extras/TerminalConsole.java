56207a4be7f7383047ea558c399dd45ed044d6c9
TimerErTim <timerertim@gmail.com>
2020-10-09 18:39:02 +0200

    add JavaDoc to TerminalConsole (constructor)

--- src/main/java/eu/timerertim/lanterna/extras/TerminalConsole.java
+++ src/main/java/eu/timerertim/lanterna/extras/TerminalConsole.java
@@ -22,6 +22,29 @@ public class TerminalConsole {
     private final boolean autoResize;
     private final KeyType skipTextAnimationKey; //This variable is null if animated println is deactivated
 
+    /**
+     * Creates a new TerminalConsole object and prepares the given {@code Screen} object.
+     * <p>
+     *  This constructor initializes the {@code TerminalConsole} object with standard values and the parameters.
+     *  It also prepares the screen by hiding the cursor, starting it (effectively entering private mode) and clearing it.
+     * </p>
+     * <p>
+     *  Standard values are:
+     *  <ul>
+     *      <li>{@code autoScrolling} = true</li>
+     *      <li>{@code autoResize} = true</li>
+     *      <li>{@code skipTextAnimationKey} = null -> which effectively means no text animation</li>
+     *  </ul>
+     * </p>
+     * <p>
+     *  After this constructor has been called the screen should not be directly modified anymore. A {@code TerminalConsole}
+     *  keeps track of the consoles content which is needed for its functionality.
+     * </p>
+     *
+     * @param screen the underlying screen
+     * @param autoUpdate the boolean describing the update behavior (comparable to auto flush)
+     * @throws IOException the {@code Exception} thrown if there is an underlying IO error when starting the screen
+     */
     public TerminalConsole(Screen screen, boolean autoUpdate) throws IOException {
         // Initialize local variables and standard values
         this.screen = screen;
@@ -41,9 +64,16 @@ public class TerminalConsole {
         graphics = screen.newTextGraphics();
     }
 
+    /**
+     * Same as calling {@link TerminalConsole#TerminalConsole(Screen, boolean) TerminalConsole(screen, true)}.
+     * <p>
+     *     Creates a new TerminalConsole with {@code autoUpdate} enabled.
+     * </p>
+     *
+     * @param screen the underlying screen
+     * @throws IOException the {@code Exception} thrown if there is an underlying IO error when starting the screen
+     */
     public TerminalConsole(Screen screen) throws IOException {
         this(screen, true);
     }
-
-
 }
