45b932f641d6c4e4fff5b1b1ba4c9e3dc6117664
TimerErTim <timerertim@gmail.com>
2020-10-09 20:05:13 +0200

    generate getter and setter methods in TerminalConsole

--- src/main/java/eu/timerertim/lanterna/extras/TerminalConsole.java
+++ src/main/java/eu/timerertim/lanterna/extras/TerminalConsole.java
@@ -10,18 +10,18 @@ import java.util.LinkedList;
 
 public class TerminalConsole {
     // Essential components
-    private final LinkedList<String> content;
     private final String[] wrappedContent;
+    private final LinkedList<String> content;
     private final Screen screen;
     private final TextGraphics graphics;
-    private final TextColor textColor;
-    private final TextColor backgroundColor;
 
     // Options
-    private final boolean autoUpdate;
-    private final boolean autoScrolling;
-    private final boolean autoResize;
-    private final KeyType skipTextAnimationKey; //This variable is null if animated println is deactivated
+    private TextColor textColor;
+    private TextColor backgroundColor;
+    private boolean autoUpdate;
+    private boolean autoScrolling;
+    private boolean autoResize;
+    private KeyType skipTextAnimationKey; //This variable is null if animated println is deactivated
 
     /**
      * Creates a new TerminalConsole object and prepares the given {@code Screen} object.
@@ -77,5 +77,76 @@ public class TerminalConsole {
      */
     public TerminalConsole(Screen screen) throws IOException {
         this(screen, true);
+    }
+
+    //TODO: Implement println
+
+    // Getter
+    public TextColor getTextColor() {
+        return textColor;
+    }
+
+    // Setter
+    public void setTextColor(TextColor textColor) {
+        this.textColor = textColor;
+    }
+
+    public TextColor getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(TextColor backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public boolean isAutoUpdate() {
+        return autoUpdate;
+    }
+
+    public void setAutoUpdate(boolean autoUpdate) {
+        this.autoUpdate = autoUpdate;
+    }
+
+    public boolean isAutoScrolling() {
+        return autoScrolling;
+    }
+
+    public void setAutoScrolling(boolean autoScrolling) {
+        this.autoScrolling = autoScrolling;
+    }
+
+    public boolean isAutoResize() {
+        return autoResize;
+    }
+
+    public void setAutoResize(boolean autoResize) {
+        this.autoResize = autoResize;
+    }
+
+    /**
+     * Returns the state of text animation.
+     * <p>
+     *     To set this value use {@link TerminalConsole#setSkipTextAnimationKey}.
+     *     It takes in a {@link KeyType} the user can use to skip the text animation. If the given {@code KeyType}
+     *     is null, that means that animated text is deactivated and this method will return false.
+     * </p>
+     *
+     * @return true if skip key is assigned, which means text is animated
+     */
+    public boolean isTextAnimated(){
+        return skipTextAnimationKey != null;
+    }
+
+    /**
+     * Sets the key used for skipping text animation and activates/deactivates said feature.
+     * <p>
+     *      Takes a {@link KeyType} that the user will use to skip text animations. If null is passed, the
+     *      feature will be deactivated.
+     * </p>
+     *
+     * @param skipTextAnimationKey null deactivates the animation for text, not null is the {@code KeyType} users have to press to skip animation
+     */
+    public void setSkipTextAnimationKey(KeyType skipTextAnimationKey) {
+        this.skipTextAnimationKey = skipTextAnimationKey;
     }
 }
