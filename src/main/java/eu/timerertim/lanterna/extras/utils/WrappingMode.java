5bc490d603f6c2e824f657c3bcefdd8581dd4b04
TimerErTim <timerertim@gmail.com>
2020-10-23 12:06:52 +0200

    Add JavaDoc to WrappingMode

--- src/main/java/eu/timerertim/lanterna/extras/utils/WrappingMode.java
+++ src/main/java/eu/timerertim/lanterna/extras/utils/WrappingMode.java
@@ -1,8 +1,37 @@
 package eu.timerertim.lanterna.extras.utils;
 
 public enum WrappingMode {
+    /**
+     * No wrapping at all.
+     * <p>
+     * Lines are printed as they are. If they are too long,
+     * the last part will not be displayed.
+     */
     NONE((line, columns) -> new String[]{line}),
+
+    /**
+     * Word based wrapping.
+     * <p>
+     * Lines, which are too long to be displayed correctly,
+     * will be word wrapped. Which means, that a word
+     * will not be ripped in half during a linebreak.
+     * Furthermore, the first character in a line
+     * will not be an empty space.
+     * If a word is too long to fit in a line, that word will be
+     * wrapped using hardwrapping. Refer to {@link WrappingMode#HARDWRAPPING}.
+     */
     SOFTWRAPPING(TextUtils::applySoftwrapping),
+
+    /**
+     * Non word based wrapping.
+     * <p>
+     * A linebreak will be inserted into the line, where it
+     * would exceed the column count. Words can be ripped in half, and
+     * if there happens to be a space after the last character, which fit
+     * in the displaying row, the next row will start with a whitespace.
+     * <p>
+     * An alternative more appealing to the eye is {@link WrappingMode#SOFTWRAPPING}.
+     */
     HARDWRAPPING(TextUtils::applyHardwrapping);
 
     private final Wrapping wrapping;
