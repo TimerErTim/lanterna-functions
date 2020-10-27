aff76f1d6418085a418b1d0842bec32fd73585f4
TimerErTim <timerertim@gmail.com>
2020-10-27 21:01:35 +0100

    WrappingTest seems to confirm the working functionality of softwrapping and ContentManager

---
+++ src/test/java/eu/timerertim/lanterna/extras/test/console/WrappingTest.java
@@ -0,0 +1,24 @@
+package eu.timerertim.lanterna.extras.test.console;
+
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import eu.timerertim.lanterna.extras.TerminalConsole;
+
+import java.io.IOException;
+
+public class WrappingTest {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        TerminalConsole console = new TerminalConsole(new DefaultTerminalFactory().createScreen(), false);
+        console.print("This text is really long and there should be softwrapping applied to it, unless I really messed up. In such a case I hope, that my PC crashes, so I don't have to work on this any longer");
+        console.update();
+        Thread.sleep(4000);
+        console.println("This text\rThe wrapped text will hopefully be replaced");
+        console.print("There is another really long text which should be wrapped based on words. Hopefully this text can also be replaced.");
+        console.update();
+        Thread.sleep(3000);
+        console.println("\rlastly that Text is removed. After 5 secs the whole console will be cleared");
+        console.update();
+        Thread.sleep(5000);
+        console.clear();
+        console.update();
+    }
+}
